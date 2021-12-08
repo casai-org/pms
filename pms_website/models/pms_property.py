d3213180d094bfc06bce6d3164a95d44a3ffbd29
Vandan-OSI <vpandeji@opensourceintegrators.com>
2021-12-08 17:41:45 +0530

    Add Google Map URL Link and Other chnages.

--- pms_website/models/pms_property.py
+++ pms_website/models/pms_property.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2021 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import werkzeug.urls
+
 from odoo import models
 
 from odoo.addons.http_routing.models.ir_http import slug
@@ -13,3 +15,13 @@ class PmsProperty(models.Model):
         for property in self:
             if property.id:
                 property.website_url = "/property/%s" % slug(property)
+
+    def google_map_link(self):
+        property_partner = self.sudo().partner_id
+        property_partner.geo_localize()
+        params = {
+            "q": "%s, %s"
+            % (property_partner.partner_latitude, property_partner.partner_longitude),
+            "z": 10,
+        }
+        return "https://maps.google.com/maps?" + werkzeug.urls.url_encode(params)
