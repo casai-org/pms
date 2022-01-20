6c7f38999df4280c9ab3b824e2b44c8d9ee262f4
Lucas Teodoro <69808339+lucas-teodorols@users.noreply.github.com>
2022-01-20 13:58:02 -0300

    Update __manifest__.py

--- connector_guesty/__manifest__.py
+++ connector_guesty/__manifest__.py
@@ -4,7 +4,7 @@
     "website": "https://github.com/OCA/pms",
     "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "depends": ["pms_base", "pms_sale", "queue_job", "sale_automatic_workflow", "payment"],
+    "depends": ["pms_base", "pms_sale", "queue_job", "sale_automatic_workflow", "payment", "crm"],
     "data": [
         "views/backend_guesty.xml",
         "views/pms_property.xml",
