8fa809c3114a3ed0487797282ed1ac2751890f8a
Jorge Luis Juarez <jorge.juarez@casai.com>
2022-02-15 19:28:23 -0600

    SDK:Migrate: Check Credentials

--- connector_guesty/models/guesty_sdk/models/account.py
+++ connector_guesty/models/guesty_sdk/models/account.py
@@ -1,12 +1,17 @@
 # Copyright (C) 2021 Casai (https://www.casai.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from ..fields import Field
 from ..models import Model
 
 
 class Account(Model):
     _name = "accounts"
-    _fields = ["name"]
+    _fields = {
+        "name": Field("name"),
+        "timezone": Field("timezone"),
+        "currency": Field("currency"),
+    }
 
     def get_account_info(self):
         return self.search_by_id("me")
