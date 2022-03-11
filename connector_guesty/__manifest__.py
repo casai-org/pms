f5b899df684fd333654aed13d50cbcc527e4d00b
Brandon Molina <brandon.molina@casai.com>
2022-03-11 16:12:05 -0600

    report pdf custom with checkint-out info

--- connector_guesty/__manifest__.py
+++ connector_guesty/__manifest__.py
@@ -20,6 +20,7 @@
         "views/pms_guesty_calendar_wizard.xml",
         "views/res_partner.xml",
         "views/sale_order_views.xml",
+        "views/sale_order_report_custom.xml",
         "wizard/pms_property_days_quotation_expiration_views.xml",
         "wizard/crm_lead_new_reservation.xml",
         "security/ir.model.access.csv",
