1428f11a4f194b41be3bd70767c21dbd4a3e7282
Jorge Luis Juarez <jorge.juarez@casai.com>
2022-05-23 12:26:45 -0500

    Fix: reduce lines lenght

--- connector_guesty/controllers/website.py
+++ connector_guesty/controllers/website.py
@@ -7,6 +7,18 @@ from odoo.http import request
 
 from odoo.addons.pms_website.controllers.website import Website
 
+DEMO_PICTURE_01 = "{}/pictures/14056/{}/{}.jpg".format(
+    "https://dl6dfy55q2gns.cloudfront.net",
+    "69af29bdd2d3b7621ca12d007fa640d1464437c7",
+    "69af29bdd2d3b7621ca12d007fa640d1464437c",
+)
+
+DEMO_PICTURE_02 = "{}/pictures/14056/{}/{}.jpg".format(
+    "https://dl6dfy55q2gns.cloudfront.net",
+    "25e4599fe67112c1daab3981b897566bfd34612f",
+    "25e4599fe67112c1daab3981b897566bfd34612",
+)
+
 
 class CasaiWebsite(Website):
     @http.route(
@@ -28,14 +40,13 @@ class CasaiWebsite(Website):
         rs = super()._prepare_property_values(pms_property, category, search, **kwargs)
         rs["slider_01"] = {
             "pictures": [
-                "https://dl6dfy55q2gns.cloudfront.net/pictures/14056/69af29bdd2d3b7621ca12d007fa640d1464437c7/69af29bdd2d3b7621ca12d007fa640d1464437c.jpg",
-                "https://dl6dfy55q2gns.cloudfront.net/pictures/14056/69af29bdd2d3b7621ca12d007fa640d1464437c7/69af29bdd2d3b7621ca12d007fa640d1464437c.jpg",
-                "https://dl6dfy55q2gns.cloudfront.net/pictures/14056/69af29bdd2d3b7621ca12d007fa640d1464437c7/69af29bdd2d3b7621ca12d007fa640d1464437c.jpg",
-                "https://dl6dfy55q2gns.cloudfront.net/pictures/14056/69af29bdd2d3b7621ca12d007fa640d1464437c7/69af29bdd2d3b7621ca12d007fa640d1464437c.jpg",
-                "https://dl6dfy55q2gns.cloudfront.net/pictures/14056/69af29bdd2d3b7621ca12d007fa640d1464437c7/69af29bdd2d3b7621ca12d007fa640d1464437c.jpg",
+                DEMO_PICTURE_01,
+                DEMO_PICTURE_02,
+                DEMO_PICTURE_01,
+                DEMO_PICTURE_02,
+                DEMO_PICTURE_01,
+                DEMO_PICTURE_02,
             ]
         }
-        rs["section_id"] = {
-            "picture": "https://dl6dfy55q2gns.cloudfront.net/pictures/14056/25e4599fe67112c1daab3981b897566bfd34612f/25e4599fe67112c1daab3981b897566bfd34612.jpg"
-        }
+        rs["section_id"] = {"picture": DEMO_PICTURE_01}
         return rs
